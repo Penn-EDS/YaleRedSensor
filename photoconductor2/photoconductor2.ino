63964720d3a3ecf268446dce04d8ab6f6299c4f8
quijanop <93285324+quijanop@users.noreply.github.com>
2022-03-04 14:39:39 -0500

    Seeing analog reads
    Put serial prints inside the loop that collects the sample.

--- photoconductor2/photoconductor2.ino
+++ photoconductor2/photoconductor2.ino
@@ -1,29 +1,30 @@
 #define Output 3
 #define Input A0
+#define readDelay 7
 
 // MH - Feb 14 2022,  update the values to match the VGA monitor from Huiazen
 // - Previous values were according to the MacBook pro
-// - Values previously programmed were 
+// - Values previously programmed were
 // int Max = 163;   // ==> 800mV
 // int Min = 86;    // ==> 420mV
 
-int Max = 40; //1023 total bits resolution. Max value for red 
+int Max = 40; //1023 total bits resolution. Max value for red
 int Min = 25; //1023 total bits resolution. Max value for red
-int in[10];   //Variable for the A0 value
 int s = 0;    //Variable to store the sum of the array
 int ave = 0;  //Variable to store ave
 int j = 0;    //Variable to iterate readings
-int nsum = 10; //Number of samples for average
+const int nsum = 10; //Number of samples for average
+int in[nsum];   //Variable for the A0 value
 
 
 
 int sumofarray(int a[],int n){
    int i,sum=0;
- 
+
     for(i=0; i<n; i++){
-         sum+=a[i];  
+         sum+=a[i];
     }
-    
+
   return sum;
  }
 
@@ -33,18 +34,27 @@ Serial.begin(9600);
 delay(200);
 
 pinMode(Output, OUTPUT);
-digitalWrite(Output,LOW);
+pinMode(Output, OUTPUT);
+digitalWrite(readDelay,LOW);
 pinMode(Input, INPUT);
 
 Serial.println("Begin");
 }
 
 void loop(){
-
+  Serial.println("Go back to beginning of loop");
+  
+  digitalWrite(readDelay, HIGH);  //Pin HIGH before making Reads
   for(j=0; j<nsum; j++){
+    //Serial.print("Sample ");  //These serial writes inside the loop introduce a significant time delay
+    //Serial.print(j + 1);
     in[j] = analogRead(Input);
-    delay(1);
+    //Serial.print(" is ");
+    //Serial.println(in[j]);
+    
   }
+
+  digitalWrite(readDelay, LOW);   //Pin LOW after making analogReads
 
   s = sumofarray(in,nsum);
   ave = s/nsum;
